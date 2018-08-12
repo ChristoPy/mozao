6a14deec1171211a4bbb6ea1bb24967541e399f0
Christopher <christo_campiglia@hotmail.com>
2018-08-12 13:46:33 -0300

    [ADD] FullScreen.

--- source/components/form.js
+++ source/components/form.js
@@ -30,6 +30,36 @@ export default {
 			Event.preventDefault ();
 
 			(this.TypedPassword === this.Password ? this.Granted = true : this.Granted = false);
+
+			this.ToggleFullScreen ();
+		},
+		ToggleFullScreen () {
+
+			if (!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement) {
+
+				if (document.documentElement.requestFullscreen) {
+
+					document.documentElement.requestFullscreen ();
+				} else if (document.documentElement.mozRequestFullScreen) {
+
+					document.documentElement.mozRequestFullScreen ();
+				} else if (document.documentElement.webkitRequestFullscreen) {
+
+					document.documentElement.webkitRequestFullscreen (Element.ALLOW_KEYBOARD_INPUT);
+				}
+			} else {
+
+				if (document.cancelFullScreen) {
+
+					document.cancelFullScreen ();
+				} else if (document.mozCancelFullScreen) {
+
+					document.mozCancelFullScreen ();
+				} else if (document.webkitCancelFullScreen) {
+
+					document.webkitCancelFullScreen ();
+				}
+			}
 		}
 	},
 	template: `
