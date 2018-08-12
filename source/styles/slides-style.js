cb3f7ae13867b727d24311d110078e5530a3e2ed
Christopher <christo_campiglia@hotmail.com>
2018-08-12 15:53:28 -0300

    [FIX][ADD] Text Not Relative To The Slide && Text With Borders.

--- source/styles/slides-style.js
+++ source/styles/slides-style.js
@@ -1,8 +1,15 @@
 export default `
+.tns-item {
+	position: relative;
+}
+
 .SlideText {
     position: absolute;
     bottom: 8px;
     left: 50%;
 	transform: translate(-50%, -50%);
+	color: #ffc107;
+	text-shadow: 2px 0 0 #000, -2px 0 0 #000, 0 2px 0 #000, 0 -2px 0 #000, 1px 1px #000, -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000;
+	
 }
 `;
