2e5577fe01e8b649a3dc0dc503b57971f29622f8
Christopher <christo_campiglia@hotmail.com>
2018-08-13 03:20:28 -0300

    [ADD] Service Worker.

--- source/main.js
+++ source/main.js
@@ -4,3 +4,11 @@ new Vue ({
 
 	render: H => H (App)
 }).$mount ("#App");
+
+
+if ("serviceWorker" in navigator) {
+
+	navigator.serviceWorker.register ("service-worker.js")
+						   .then (Registration => console.log (`ServiceWorker Registratered. ${Registration.scope}`))
+						   .catch (SomeError => console.log (`ServiceWorker Registration Failed: ${SomeError}`));
+}
---
+++ source/service-worker.js
@@ -0,0 +1,29 @@
+importScripts ("helpers/CachePolyfill.js");
+
+
+const APP_VERSION = "1.0.0";
+const CACHE_NAME = `ChristoPy - Mozao : Version ${APP_VERSION}`;
+
+const ROUTES = [
+
+	"/",
+	"index.html",
+	"dependencies/bootstrap.min.css",
+	"dependencies/tiny-slider.css",
+	"dependencies/vue.js",
+	"dependencies/jquery-3.3.1.slim.min.js",
+	"dependencies/bootstrap.bundle.min.js",
+	"dependencies/tiny-slider.js",
+	"main.js",
+	"components/app.js",
+	"components/form.js",
+	"components/slides.js",
+	"components/style-component.js",
+	"styles/app-style.js",
+	"styles/form-style.js",
+	"styles/slides-style.js"
+];
+
+
+self.addEventListener ("install", Event => Event.waitUntil (caches.open (CACHE_NAME).then (Cache => Cache.addAll (ROUTES))));
+self.addEventListener ("fetch", Event => Event.respondWith (caches.match (Event.request).then (Response => Response || fetch (Event.request))));
